2744617c719fb30b1b603eb6128807b4abd269f5
TarunZoro <tarp2605@gmail.com>
2024-04-17 10:28:08 +0530

    Connecting to backend P2

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://edw-project-v1-1.onrender.com/",
         methods: ["GET", "POST"],
     },
 });
