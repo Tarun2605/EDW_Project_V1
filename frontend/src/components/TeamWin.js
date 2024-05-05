ca192c6e6463830772c5924d4ab3ad3e3864d2de
TarunZoro <tarp2605@gmail.com>
2024-05-05 08:41:32 +0530

    correctionn

--- frontend/src/components/TeamWin.js
+++ frontend/src/components/TeamWin.js
@@ -1,28 +1,13 @@
 import './TeamWin.css';
-export default function TeamWin(props) {
-    return (
-        props.teamWin =="A" ? (
-            <div className="TableTennis-score-board-cont team-win-cont" style={{
-                backgroundColor: props.teamWin === "A" ? 'lightblue' : 'lightcoral'
-            }}>
-                Team {props.teamWin} wins!
-            </div>
-        ) :
-        props.teamWin =="B" ? 
-        (
-            <div className="TableTennis-score-board-cont team-win-cont" style={{
-                backgroundColor: props.teamWin === "A" ? 'lightblue' : 'lightcoral'
-            }}>
-                Team {props.teamWin} wins!
-            </div>
-        ) :
-        props.teamWin =="Both" ?
-        (
-            <div className="TableTennis-score-board-cont team-win-cont">
-                It's a draw!
-            </div>
-        )
-        :
-        <></>
-    );
+export default function TeamWin(props){
+return (
+    <div className="TableTennis-score-board-cont team-win-cont" style={
+            {
+                    backgroundColor: props.team == 'A' ? 'lightblue' : 'lightcoral'
+            }
+            
+    }>
+        Team {props.team} wins!
+    </div>
+)
 };
